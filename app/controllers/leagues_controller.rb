e792cbc0b661bb2b417f606217b57048408fef3b
Frantchesco Cecchin <fcecchin@gmail.com>
2014-05-23 22:46:15 -0300

    fix trust params

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -29,7 +29,6 @@ class LeaguesController < ApplicationController
     respond_to do |format|
       if @league.save
         format.html { redirect_to @league, notice: 'League was successfully created.' }
-        format.json { render :show, status: :created, location: @league }
       else
         format.html { render :new }
         format.json { render json: @league.errors, status: :unprocessable_entity }
@@ -69,6 +68,6 @@ class LeaguesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def league_params
-      params[:name]
+      params.require(:league).permit(:name)
     end
 end
